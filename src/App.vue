30a38f698ba8f13e55e298ca07018e9291c103ef
karlge <karlge_bear@163.com>
2021-09-21 21:31:34 +0800

    study stylus and vue-cli-4

---
+++ src/App.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <h1>App 组件</h1>
+    <button class="btn btn1">按钮1</button>
+    <button class="btn btn2">按钮2</button>
+    <ul>
+      <li @click="choose(index)" :class="{active: navIndex === index}" v-for="item,index in 20" :key="index">{{index+1}}</li>
+    </ul>
+  </div>
+</template>
+
+<script type='text/ecmascript-6'>
+export default {
+  data () {
+    return {
+      navIndex: 0
+    }
+  },
+  methods:{
+    choose(index){
+      this.navIndex = index
+    }
+  }
+}
+
+</script>
+<style lang="stylus" rel="stylesheet/stylus">
+  /* 定义变量 */
+  $color = green
+  /* 定义混合 */
+  btnStyle(w,h,bg)
+    width w
+    height h
+    border none
+    background-color bg
+  
+  /* 定义一个函数 */
+  add(a,b)
+    a+b
+
+  h1
+    color $color
+  .btn1
+    btnStyle(100px,100px,red)
+    box-sizing content-box
+    padding add(25px,25px)
+  .btn2
+    btnStyle(200px,200px,$color)
+  .box 
+    width 200px
+    height 200px
+    border 1px solid black
+  ul
+    li
+     line-height 50px
+    li.active 
+      color red
+</style>
---
+++ src/main.js
@@ -0,0 +1,10 @@
+import Vue from 'vue'  
+import App from './App.vue'
+
+
+Vue.config.productionTip = false
+new Vue({
+  render: h => h(App),  //渲染函数接收一个 createElement 方法作为第一个参数用来创建 VNode(创建DOM节点，插入html)
+}).$mount('#app')
+
+console.log('xxx')
